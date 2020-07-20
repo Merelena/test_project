5f3cf1a156bb65747c4ad1a02334b986025f9b59
Elena Fyodorova <47062507+Merelena@users.noreply.github.com>
2020-07-20 17:32:10 +0300

    Extra imports have been removed.

--- test_app/views.py
+++ test_app/views.py
@@ -1,6 +1,4 @@
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView, ListAPIView
 from .models import Book
 from .serializers import BookSerializer, OneBookSerializer, BookCreateSerializer
 from rest_framework import viewsets
